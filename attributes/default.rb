98c5774352be64fb4aaf5116539873a161f8095e
MelHopper <MelHopper@users.noreply.github.com>
2015-07-27 09:30:03 +0100

    ID editing
    Corrected first instance of use of abbreviation for Installation Manager. Further clarity to attribute definitions.

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,7 +18,7 @@ default[:im][:group] = 'im-admin'
 
 #<
 # Home directory for +im user+. The attribute is ignored if +im user+ is root. 
-# For nonAdmin access mode, IBM Installation 'Manager (IM)'s registry is found at +user_home_dir/etc/.ibm/registry/InstallationManager.dat+
+# For nonAdmin access mode, the registry of IBM Installation Manager (IM) is found at +user_home_dir/etc/.ibm/registry/InstallationManager.dat+
 # The registry path MUST NOT be equal to a parent directory or a subdirectory of +base_dir+
 #>
 default[:im][:user_home_dir] = '/home/im'
@@ -29,12 +29,12 @@ default[:im][:base_dir] = '/opt/IBM/InstallationManager'
 default[:im][:data_dir] = '/var/ibm/InstallationManager'
 
 #<
-# The IM install zip file. Set this if the installer is on a local filesystem.
+# The IM install zip file. Set this attribute if the installer is on a local filesystem.
 #>
 default[:im][:install_zip][:file] = nil
 
 #<
-# The IM install zip url. Set this if the installer is on a remote fileserver.
+# The IM install zip url. Set this attribute if the installer is on a remote fileserver.
 #>
 default[:im][:install_zip][:url] = nil
 
