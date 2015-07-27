2a15e26bf301379dea8c4985b4121bba22c5973a
MelHopper <MelHopper@users.noreply.github.com>
2015-07-27 09:57:19 +0100

    ID editing
    Adding formatting to attribute and file names.

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,20 +12,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 
-#<> User and Group name under which the server will be installed and running.
+#<> User and group name under which the server will be installed and running.
 default[:im][:user] = 'im'
 default[:im][:group] = 'im-admin'
 
 #<
-# Home directory for +im user+. The attribute is ignored if +im user+ is root. 
-# For nonAdmin access mode, the registry of IBM Installation Manager (IM) is found at +user_home_dir/etc/.ibm/registry/InstallationManager.dat+
-# The registry path MUST NOT be equal to a parent directory or a subdirectory of +base_dir+
+# Home directory for `im user`. The attribute is ignored if `im user` is root. 
+# For nonAdmin access mode, the registry of IBM Installation Manager (IM) is found at `user_home_dir/etc/.ibm/registry/InstallationManager.dat`.
+# The registry path MUST NOT be equal to a parent directory or a subdirectory of `base_dir`.
 #>
 default[:im][:user_home_dir] = '/home/im'
 
 #<> Base installation directory.
 default[:im][:base_dir] = '/opt/IBM/InstallationManager'
-#<> Data directory
+#<> Data directory.
 default[:im][:data_dir] = '/var/ibm/InstallationManager'
 
 #<
