2a5da08b148f61766dd337651b6d0fe71b5bf134
Jeremy Hughes <jpjhughes@gmail.com>
2015-11-03 17:08:01 +0000

    Better description field

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'iim'
 maintainer       'IBM'
 maintainer_email ''
 license          'Apache 2.0'
-description      'Installs/Configures im'
+description      'Installs, configures and runs IBM Installation Manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.4.0'
 
